c0b5f20aefbf1d7cc03e02dd991426e31849d404
Babis <cwbabis@gmail.com>
2019-05-03 20:43:35 -0700

    Grew Burger Seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Burger1');
+INSERT INTO burgers (name) VALUES ('Burger2');
+INSERT INTO burgers (name, eatan) VALUES ('Burger3', true);
+INSERT INTO burgers (name, eatan) VALUES ('Burger4', true);
+INSERT INTO burgers (name, eatan) VALUES ('Burger5', true);
+INSERT INTO burgers (name) VALUES ('Burger6');
